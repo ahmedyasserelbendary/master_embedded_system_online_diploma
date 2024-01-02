60715f0e3de86da900483c1bc366e058637cd26b
ahmed yasser elbendary <ahmedyasserelbendary3@gmail.com>
2024-01-02 23:46:20 +0200

    Write C Program to Find ASCII Value of a Character

---
+++ c/lec 3/ex_5.c
@@ -0,0 +1,10 @@
+// Write C Program to Find ASCII Value of a Character
+#include<stdio.h>
+int main ()
+{
+    char character =0;
+    printf("enter a character  :");
+    scanf("%c",&character);
+    printf("\nASCII value of %c = %d ",character,character);
+    return 0;
+}
