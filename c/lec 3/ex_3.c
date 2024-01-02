2ba64725666a1ed5427a7292a133fd0b47039385
ahmed yasser elbendary <ahmedyasserelbendary3@gmail.com>
2024-01-02 23:43:25 +0200

    Write C Program to Add Two Integers

---
+++ c/lec 3/ex_3.c
@@ -0,0 +1,13 @@
+// Write C Program to Add Two Integers
+#include <stdio.h>
+int main()
+{
+    int num_1=0;
+    int num_2=0;
+    int sum=0;
+    printf("enter two integer number :");
+    scanf("%d %d",&num_1,&num_2);
+    sum=num_1 +num_2;
+    printf("\n sum = %d \n",sum);
+    return 0;
+}
