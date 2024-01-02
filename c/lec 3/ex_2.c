537aeaa109d9e7afa5b00d9113c50a6725f19d7f
ahmed yasser elbendary <ahmedyasserelbendary3@gmail.com>
2024-01-02 23:42:03 +0200

    Write C Program to Print a Integer Entered by a User

---
+++ c/lec 3/ex_2.c
@@ -0,0 +1,12 @@
+//Write C Program to Print a Integer Entered by a User
+#include <stdio.h>
+int main()
+{
+    int i=0;
+    printf("Enter a integer :");
+    scanf("%d",&i);
+    printf("\n you entered : %d",i);
+
+
+    return 0;
+}
