3fb91ab5a66f0a2523322ab65940f308a24c4b11
ahmed yasser elbendary <ahmedyasserelbendary3@gmail.com>
2024-01-05 15:23:25 +0200

    write c program to calculate the summation of two numbers

---
+++ c/lec 3/ex_13.c
@@ -0,0 +1,19 @@
+
+// c program to calculate sum of natural numbers
+#include<stdio.h>
+int main()
+{
+    int num_1=0;
+    int num_2=0;
+    int sum=0;
+    printf("pleas enter two numbers to sum\n");
+    printf("enter the first number :");
+    scanf("%d",&num_1);
+    printf("enter the second number :");
+    scanf("%d",&num_2);
+    sum= num_1 + num_2 ;
+    printf("\n sum = %i\n",sum);
+
+
+    return 0;
+}
