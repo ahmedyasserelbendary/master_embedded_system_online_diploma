3fbc6373c69dd775d324aeb11d29103968ffc43f
ahmed yasser elbendary <ahmedyasserelbendary3@gmail.com>
2024-01-05 15:13:22 +0200

    write c program to check character is alphabet or not

--- c/lec 3/ex_12.c
+++ c/lec 3/ex_12.c
@@ -5,40 +5,11 @@ int main()
   char character=0;
     printf("enter your number :");
     scanf("%c",&character);
-    switch(character)
-    {
-
-    case 'a' :
-    case 'b' :
-    case 'c' :
-    case 'd' :
-    case 'e' :
-    case 'f' :
-    case 'g' :
-    case 'h' :
-    case 'i' :
-    case 'j' :
-    case 'k' :
-    case 'l' :
-    case 'm' :
-    case 'n' :
-    case 'o' :
-    case 'p' :
-    case 'q' :
-    case 'r' :
-    case 's' :
-    case 't' :
-    case 'u' :
-    case 'v' :
-    case 'w' :
-    case 'x' :
-    case 'y' :
-    case 'z' :
-        printf(" %c is alphabet ",character);
-        break;
-    default :
+    if ( (character>=97 && character <= 122) || (character>=65 && character <= 90)  ) // equevlant character in ascii table
+         printf(" %c is alphabet ",character);
+    else
         printf(" %c is not alphabet ",character);
-    }
+
     return 0;
 }
 
