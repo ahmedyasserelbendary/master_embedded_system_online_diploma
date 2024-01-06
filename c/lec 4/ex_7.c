f2b667f0fe651e05b2fe94476fbcc9c377d716bd
ahmedyasserelbendary <153929045+ahmedyasserelbendary@users.noreply.github.com>
2024-01-06 09:59:47 +0200

    Add files via upload

---
+++ c/lec 4/ex_7.c
@@ -0,0 +1,17 @@
+// c program to find the length of a string
+#include<stdio.h>
+int main()
+{
+   char array[100];
+    int i=0;
+    int n=0;
+    printf("enter a string :");
+    gets(array);
+     while(array[i] !=0 )   // strlen() function
+    {
+        n++;
+        i++;
+    }
+    printf("\nlength of string : %d\n",n);
+    return 0;
+}
---
+++ c/lec 4/ex_8.c
@@ -0,0 +1,24 @@
+// c program to reverse string without using library function
+#include<stdio.h>
+int main()
+{
+   char a[100];
+   char b[100];
+    int i=0;
+    int n=0;
+    int j=0;
+    printf("enter a string :");
+    gets(a);
+    n=strlen(a);
+    for(i=0,j=n-1;i<n&&j>=0;i++,j--)
+    {
+        b[i]=a[j];
+    }
+    printf("\n%s\n",a);
+    printf("\n=====================\n");
+    printf("\n%s\n",b);
+
+
+
+    return 0;
+}
