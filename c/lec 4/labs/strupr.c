ab2c826e37ef741d0423e6b0b02128de7a7ad0c3
ahmedyasserelbendary <153929045+ahmedyasserelbendary@users.noreply.github.com>
2024-01-07 00:14:21 +0200

    Add files via upload

---
+++ c/lec 4/labs/strupr.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100];
+    int i=0;
+    int n=0;
+    printf("enter a string :");
+    gets(a);
+    n=strlen(a);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=97 && a[i]<=122)
+            a[i]-=32;
+    }
+    printf("\n%s\n",a);
+
+    return 0;
+}
