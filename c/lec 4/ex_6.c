34228c27b65a61bda7869dd0d06fe8719be45a3f
ahmedyasserelbendary <153929045+ahmedyasserelbendary@users.noreply.github.com>
2024-01-06 09:43:28 +0200

    Add files via upload

---
+++ c/lec 4/ex_6.c
@@ -0,0 +1,33 @@
+// cprogram to find thhe frequency of character in a string
+#include<stdio.h>
+#include<string.h>
+int main ()
+{
+    char array[100];
+    int i=0;
+    int n=0;
+    int repeated=0;
+    char character=0;
+    printf("enter a string :");
+    gets(array);
+    printf("enter a character to find frequency :");
+    scanf("%c",&character);
+    n=strlen(array);
+    /*while(array[i] !=0 )   // strlen() function
+    {
+        n++;
+        i++;
+    }*/
+    printf("\n%d\n",n);
+    for(i=0;i<n;i++)
+    {
+        if(array[i]==character)
+         {
+              repeated++;
+         }
+
+    }
+    printf("\nfrequency of %c  = %d\n",character,repeated);
+
+    return 0;
+}
