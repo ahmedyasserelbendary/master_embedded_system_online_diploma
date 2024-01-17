afd185565407b56a930539486f9e4b01345ee837
ahmedyasserelbendary <153929045+ahmedyasserelbendary@users.noreply.github.com>
2024-01-17 23:43:06 +0200

    Add files via upload

---
+++ c/lec 5/exp_2.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+int main()
+{
+    int a=0;
+    int x=0;
+    printf("please enter the number :");
+    scanf("%d",&a);
+    if(a>=0)
+        x=factorial(a);
+    else
+        printf("\n !!!! undefine number\n");
+    printf("\nfactorial is %d\n",x);
+
+
+
+    return 0;
+}
+int factorial(int a)
+{
+    if(a==0)
+        return 1;
+    else
+        return a*factorial(a-1);
+}
