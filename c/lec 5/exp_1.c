66f9a3a9b05131f798e399bb273948d5f06c2b8d
ahmedyasserelbendary <153929045+ahmedyasserelbendary@users.noreply.github.com>
2024-01-17 23:33:09 +0200

    Add files via upload

---
+++ c/lec 5/exp_1.c
@@ -0,0 +1,40 @@
+//c program to display prime number between any interval
+#include<stdio.h>
+void prime(void);
+
+int main()
+{
+
+
+    prime();
+    return 0;
+}
+void prime(void)
+{
+    int a=0;
+    int b=0;
+    printf("please enter the intervals :");
+    fflush(stdin);
+    scanf("%d",&a);
+    fflush(stdin);
+    scanf("%d",&b);
+    fflush(stdin);
+    printf("the prime number between interval [%d , %d] :\n",a,b);
+
+	int i=0;
+    for(;b>=a;a++)
+    {
+        for(i=2;i<a;i++)
+        {
+            int x=0;
+            x=a%i;
+            if(x==0)
+            {
+                break;
+            }
+
+        }
+        if((a-i)==0)
+        printf("%d  ",a);
+    }
+}
