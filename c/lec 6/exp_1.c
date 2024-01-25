b5a9f1ef087610a6a38e9442691cf065f1575b59
ahmedyasserelbendary <153929045+ahmedyasserelbendary@users.noreply.github.com>
2024-01-25 16:26:38 +0200

    Add files via upload

---
+++ c/lec 6/exp_1.c
@@ -0,0 +1,34 @@
+/*
+  write c program to store information (names,rolls and marks) of a students using structre
+  user enter the information
+*/
+#include<stdio.h>
+struct student_info
+{
+    char name[50];
+    int roll;
+    float mark;
+};
+struct student_info ahmed;
+int main()
+{
+    printf("enter the name of student :");
+    gets(ahmed.name);
+    printf("\nenter the roll os student :");
+    scanf("%d",&ahmed.roll);
+    printf("\nenter the mark os student :");
+    scanf("%f",&ahmed.mark);
+
+    printf("\nname : %s\n",ahmed.name);
+    printf("\nroll = %d\n",ahmed.roll);
+    printf("\nmark= %0.2f\n",ahmed.mark);
+
+
+
+
+
+
+
+
+    return 0;
+}
